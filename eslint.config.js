96ed6f2a2d4f7cc1952559713f04c543e6e01c9d
JorkDev <153477451+mundstockGG@users.noreply.github.com>
2025-06-20 15:05:11 -0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -16,7 +16,7 @@ export default [
     },
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-explicit-any": "error",
       "@typescript-eslint/explicit-function-return-type": "off",
     },
   },
